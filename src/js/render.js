147d2458e1c97a84166419df170cc7c18f495dff
David Peterka <143167841+DavidPeterK@users.noreply.github.com>
2024-03-30 06:07:05 +0100

    update load animation

--- src/js/render.js
+++ src/js/render.js
@@ -87,16 +87,23 @@ function nameRule(i) {
 
 // Funktion, um die Bilder verschwinden zu lassen
 function hideImages() {
+
     document.querySelector('.light-container').classList.add('d-none');
     document.querySelector('.loading-img-top').classList.add('hide-top');
     document.querySelector('.loading-img-bottom').classList.add('hide-bottom');
+    setTimeout(() => {
+        document.querySelector('.loading-screen').classList.add('d-none');
+    }, 1600);
+
 }
 
 // Funktion, um die Bilder wieder anzuzeigen
 function showImages() {
+    const main = document.querySelector('.loading-screen');
     const pikaImg = document.querySelector('.light-container');
     const topImg = document.querySelector('.loading-img-top');
     const bottomImg = document.querySelector('.loading-img-bottom');
+    main.classList.remove('d-none');
     topImg.classList.remove('hide-top');
     bottomImg.classList.remove('hide-bottom');
     topImg.classList.add('show-top');
@@ -105,6 +112,6 @@ function showImages() {
         pikaImg.classList.remove('d-none');
         topImg.classList.remove('show-top');
         bottomImg.classList.remove('show-bottom');
-    }, 2900); // Stelle sicher, dass dies der Dauer deiner Animation entspricht
+    }, 1500); // Stelle sicher, dass dies der Dauer deiner Animation entspricht
 }
 
